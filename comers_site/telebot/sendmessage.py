8d9c6afba366a3a264b81fbb91d5e8ccb7c98204
lx3S <devforg9@gmail.com>
2023-11-30 21:42:58 +0200

    upload the project

---
+++ comers_site/telebot/sendmessage.py
@@ -0,0 +1,46 @@
+import requests
+from .models import TeleSetitings
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+
+token =  os.environ.get('TOKEN')
+chat_id = os.environ.get('CHAR_ID')
+
+def sendTelegram(tg_name, tg_phone):
+    if TeleSetitings.objects.get(pk=1):
+        settings = TeleSetitings.objects.get(pk=1)
+        token = str(settings.tg_token)
+        chat_id = str(settings.tg_chat)
+        text = str(settings.tg_message)
+        api = 'https://api.telegram.org/bot'
+        method = api + token + '/sendMessage'
+
+        if text.find('{') and text.find('}') and text.rfind('{') and text.rfind('}'):
+            part_1 = text[0:text.find('{')]
+            part_2 = text[text.find('}')+1:text.rfind('{')]
+            part_3 = text[text.rfind('}'):-1]
+            text_slise = part_1 + tg_name + part_2 + tg_phone + part_3
+        else:
+            text_slise = text
+
+        try:
+            req = requests.post(method, data={
+                'chat_id':chat_id,
+                'text':text_slise
+                })
+        except:
+            pass
+
+        finally:
+            if req.status_code != 200:
+                print('Sending error')
+            elif req.status_code == 500:
+                print('Error 500')
+            else:
+                print('All good - message sent!')
+
+    else:
+        pass
